79f68de705811710e60a0221dfe0e5f672c8964c
EkramjemalH <ekramjemalh@gmail.com>
2025-10-01 13:31:25 +0300

    word counter

---
+++ wordcounter.js
@@ -0,0 +1,7 @@
+/*Write a program that takes a sentence and counts how many words it contains.
+
+let sentence = "JavaScript is a powerful programming language";
+// Output: 6 words*/
+let sentence = "JavaScript is a powerful programming language";
+sentence=sentence.split(" ");
+console.log(sentence.length);
